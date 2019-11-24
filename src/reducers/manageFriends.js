376f880599f92c9c1111a4df72c06b9d03525e96
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-24 02:16:26 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,6 +2,8 @@ export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
       return {friends: state.friends.concat(action.friend)}
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => friend.id !== action.id)}
     default:
       return {friends: []}
   }
