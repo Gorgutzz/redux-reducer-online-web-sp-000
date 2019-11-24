7a0b0db5670d3ac0a9ba261f24144a9a368e7b4e
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-24 02:12:55 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state, action){
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
     case 'DECREASE_COUNT':
       return {count: state.count - 1}
     default:
